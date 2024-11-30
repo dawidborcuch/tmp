bfc765eee67bee6798668d09f057834d6c6a264d
Dawid B0 <dawid.borcuch@gmail.com>
2024-11-30 11:36:30 +0100

    slowniki

--- zjazd02/1_slowniki.py
+++ zjazd02/1_slowniki.py
@@ -1,3 +1,22 @@
 
 
-lista_zakupow = ['marchew', 'ryz', 'woda']
+slownik_zakupow = {'marchew' : 3, 'ryz' : 5, 'woda' : 3}
+
+print(slownik_zakupow['marchew'])
+print(slownik_zakupow.values())
+
+slownik_zakupow['serek'] = 4
+slownik_zakupow['marchew'] += 1
+print(slownik_zakupow)
+
+for produkt in slownik_zakupow:
+    print(f' chcesz kupić {produkt} w ilosci {slownik_zakupow[produkt]}')
+
+for produkt in slownik_zakupow.keys():
+    print(f' chcesz kupić {produkt} w ilosci {slownik_zakupow[produkt]}')
+
+for cena in slownik_zakupow.values():
+    print(cena)
+
+for produkt, cena in slownik_zakupow.items():
+    print(produkt, cena)
